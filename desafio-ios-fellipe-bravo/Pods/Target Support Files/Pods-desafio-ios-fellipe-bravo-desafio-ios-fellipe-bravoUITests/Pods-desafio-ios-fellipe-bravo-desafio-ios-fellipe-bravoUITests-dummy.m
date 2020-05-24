2577f920f865823d9a71f3db04fa9887822c3623
insidemybrain <fellipe.bravo@gmail.com>
2020-05-24 15:30:49 -0300

    [Structure] Added Cocoapods

---
+++ desafio-ios-fellipe-bravo/Pods/Target Support Files/Pods-desafio-ios-fellipe-bravo-desafio-ios-fellipe-bravoUITests/Pods-desafio-ios-fellipe-bravo-desafio-ios-fellipe-bravoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_desafio_ios_fellipe_bravo_desafio_ios_fellipe_bravoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_desafio_ios_fellipe_bravo_desafio_ios_fellipe_bravoUITests
+@end
---
+++ desafio-ios-fellipe-bravo/Pods/Target Support Files/Pods-desafio-ios-fellipe-bravoTests/Pods-desafio-ios-fellipe-bravoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_desafio_ios_fellipe_bravoTests : NSObject
+@end
+@implementation PodsDummy_Pods_desafio_ios_fellipe_bravoTests
+@end
